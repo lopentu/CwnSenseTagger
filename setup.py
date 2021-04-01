543bf127efc4682e57c3190b1e077a16c59a173f
seantyh <seantyh@gmail.com>
2021-04-01 18:50:33 +0800

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
     name="CwnSenseTagger", 
-    version="0.1.3",
+    version="0.1.4",
     author="NTUGIL LOPE Lab",    
     description="A package to use chinese word net to achieve word sense disambigution task",
     long_description=long_description,
